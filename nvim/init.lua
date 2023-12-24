3c2a4ddb943f4a1f9c86272dbf9cb6f0b357afb2
Gabriel Goffart <gab.goffart@gmail.com>
2023-12-24 10:30:39 -0500

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,45 +1,99 @@
-require'packer'.startup(function(use)
-    use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    use 'echasnovski/mini.nvim'
-
-    use 'kyazdani42/nvim-web-devicons'
-    use 'neovim/nvim-lspconfig'
-
-    use 'karb94/neoscroll.nvim'
-
-    use 'nvim-treesitter/nvim-treesitter'
-
-    use 'lfe-support/vim-lfe'
-
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
+require('lazy').setup({
+  'ionide/Ionide-vim',
+  'echasnovski/mini.nvim',
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
     }
-
-    use 'ionide/Ionide-vim'
-
-
-end)
+  },
+  'folke/trouble.nvim',
+  'nvim-tree/nvim-web-devicons',
+  'neovim/nvim-lspconfig',
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  'dcampos/nvim-snippy',
+  'dcampos/cmp-snippy',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/nvim-cmp',
+  'declancm/cinnamon.nvim',
+  'rebelot/kanagawa.nvim',
+  'AlexvZyl/nordic.nvim',
+  {
+    'olivercederborg/poimandres.nvim',
+    config = function()
+      require('poimandres').setup()
+    end
+  },
+}, {
+  version = nil,
+})
 
 vim.cmd([[let g:fsharp#lsp_auto_setup = 0]])
 
 vim.g.mapleader = ' '
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
-require'neoscroll'.setup()
+require 'mini.comment'.setup({})
+require 'mini.tabline'.setup({})
+require 'mini.statusline'.setup({})
+require 'mini.pairs'.setup({})
 
-require'mini.comment'.setup({})
-require'mini.tabline'.setup({})
-require'mini.surround'.setup({})
-require'mini.statusline'.setup({})
-require'mini.pairs'.setup({})
-require'mini.completion'.setup({})
+require 'cinnamon'.setup()
 
 -- CONFIGS
-require'settings'
-require'treesitter'
-require'lsp'
-require'keymaps'
-local palette = require'colors.base16-chalk'
-require'mini.base16'.setup({palette=palette, use_cterm=true})
+require 'settings'
+require 'treesitter'
+require 'lsp'
+require 'keymaps'
+require 'completion'
+
+require 'telescope'.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<esc>'] = require 'telescope.actions'.close
+      },
+    },
+  }
+}
+
+require 'trouble'.setup()
+require 'noice'.setup({
+  routes = {
+    {
+      -- this is needed to filter out excessive lsp progress messages from Ionide(F#)
+      -- we just check if the message contains .fs extension and skip it
+      filter = {
+        event = "lsp",
+        kind = "progress",
+        find = ".*.fs.*",
+      },
+      opts = { skip = true },
+    },
+  },
+})
+
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.templ' },
+  command = 'setfiletype templ',
+})
+
+vim.cmd.colorscheme 'nordic'
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,35 +1,49 @@
 local nvim_lsp = require('lspconfig')
+local trouble = require('trouble')
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- on_attach is a function run every time an LSP server attaches to the current buffer.
 -- It should setup special keybindings, etc.
-local on_attach = function(_, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+local function on_attach(_, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Keybindings for LSP
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', '<leader>sD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', '<leader>sd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>sh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<leader>sf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    buf_set_keymap('n', '<leader>si', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<leader>ss', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<leader>sa', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<leader>sr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>sR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<leader>sp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', '<leader>sn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-
-    vim.api.nvim_command [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+  -- Keybindings for LSP
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set('n', '<leader>sD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', '<leader>sd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', '<leader>sh', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>sf', function() vim.lsp.buf.format({ async = true }) end, opts)
+  vim.keymap.set('n', '<leader>si', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<leader>ss', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<leader>sa', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>sr', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>sR', function() trouble.toggle('lsp_references') end, opts)
+  vim.keymap.set('n', '<leader>xx', function() trouble.toggle('workspace_diagnostics') end, opts)
+  vim.keymap.set('n', '<leader>xq', function() trouble.toggle('quickfix') end, opts)
 end
 
--- setup lsps from the `servers` table
-local servers = {'pylsp', 'rust_analyzer', 'tsserver', 'gopls', 'svelte', 'dartls', 'fsautocomplete'}
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
+
+local function setup(server)
+  server.setup {
     on_attach = on_attach,
-    flags = {debounce_text_changes = 150}
+    flags = { debounce_text_changes = 150 },
+    -- capabilities = capabilities,
   }
 end
+
+-- init ionide first
+vim.cmd [[let g:fsharp#lsp_auto_setup = 0]]
+
+-- setup lsps from the `servers` table
+local servers = { 'pylsp', 'rust_analyzer', 'tsserver', 'gopls', 'svelte', 'dartls', 'kotlin_language_server', 'lua_ls',
+  'clojure_lsp', 'ocamllsp', 'templ', 'fennel_ls' }
+
+for _, lsp in ipairs(servers) do
+  setup(nvim_lsp[lsp])
+end
+
+setup(require('ionide'))
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, { focusable = false }
+)
